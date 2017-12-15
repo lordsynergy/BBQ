c6e6d3ba1210effe86820c2c3262e0ce6fd89224
lordsynergy <djordj88@mail.ru>
2017-12-15 22:55:49 +0300

    events initial

---
+++ app/models/event.rb
@@ -0,0 +1,24 @@
+# Модель события
+class Event < ApplicationRecord
+  # Событие принадлежит юзеру
+  belongs_to :user
+
+  # Юзера не может не быть. Обратите внимание, что в rails 5 связи валидируются
+  # по умолчанию
+  # У события много комментариев и подписок
+  has_many :comments, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
+
+  # У события много подписчиков (объекты User), через таблицу subscriptions,
+  # по ключу user_id
+  has_many :subscribers, through: :subscriptions, source: :user
+
+  validates :user, presence: true
+
+  # Заголовок должен быть, и не может быть длиннее 255 букв
+  validates :title, presence: true, length: {maximum: 255}
+
+  # Также у события должны быть заполнены место и время проведения
+  validates :address, presence: true
+  validates :datetime, presence: true
+end
---
+++ config/routes.rb
@@ -0,0 +1,14 @@
+Rails.application.routes.draw do
+  devise_for :users
+  root "events#index"
+
+  resources :events do
+    # вложенный ресурс комментов
+    resources :comments, only: [:create, :destroy]
+
+    # вложенный ресурс подписок (см. rake routes)
+    resources :subscriptions, only: [:create, :destroy]
+  end
+
+  resources :users, only: [:show, :edit, :update]
+end
