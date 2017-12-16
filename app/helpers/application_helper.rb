7ca7c5a5b910bf851b70a303b299a1b9339516fd
lordsynergy <djordj88@mail.ru>
2017-12-16 21:59:43 +0300

    add subscription to event

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,10 @@
 module ApplicationHelper
+  # Возвращает путь к аватарке данного юзера
+  def user_avatar(user)
+    asset_path('user.png')
+  end
+
+  def fa_icon(icon_class)
+    content_tag 'span', '', class: "fa fa-#{icon_class}"
+  end
 end
